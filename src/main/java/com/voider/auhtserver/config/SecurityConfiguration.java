1e2c991597ee62bbc1a3105908d372d745d28ba1
Edgar Xolop <edgarxoix@gmail.com>
2019-08-30 00:08:27 -0600

    add endpoint to get the user auth info

--- src/main/java/com/voider/auhtserver/config/SecurityConfiguration.java
+++ src/main/java/com/voider/auhtserver/config/SecurityConfiguration.java
@@ -15,11 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/voider/auhtserver/controller/TestAuth.java
+++ src/main/java/com/voider/auhtserver/controller/TestAuth.java
@@ -1,17 +1,19 @@
 package com.voider.auhtserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import java.security.Principal;
 
 @EnableAuthorizationServer
 @EnableResourceServer
 @RestController
 public class TestAuth {
 
-    @RequestMapping(value = "/products")
-    public String getProductName() {
-        return "Honey";
+    @GetMapping("/me")
+    public Principal user(Principal principal) {
+        return principal;
     }
 }
