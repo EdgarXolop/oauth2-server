4c11135580876ca5fc12d58e88a81ae1b84e4796
Edgar Xolop <edgarxoix@gmail.com>
2019-09-11 02:00:43 -0600

    minor fixes

--- src/main/java/com/voider/auhtserver/config/CustomUser.java
+++ src/main/java/com/voider/auhtserver/config/CustomUser.java
@@ -3,10 +3,10 @@ package com.voider.auhtserver.config;
 import com.voider.auhtserver.model.user.User;
 
 public class CustomUser extends org.springframework.security.core.userdetails.User {
+
     private static final long serialVersionUID = 1L;
+
     public CustomUser(User user) {
         super(user.getUser_name(), user.getPassword(), user.getGrantedAuthoritiesList());
-        System.out.println(user.getUser_name());
-        System.out.println(user.getPassword());
     }
 }
--- src/main/java/com/voider/auhtserver/config/OAuth2Config.java
+++ src/main/java/com/voider/auhtserver/config/OAuth2Config.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -15,6 +16,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     private String clientid = "acme";
     private String clientSecret = "acmesecret";
